ab50858b20fc414c6f6623b431d96079b604669b
Pedroordepw <89821646+Pedroordepw@users.noreply.github.com>
2024-02-19 01:12:55 -0300

    Delete backend directory

--- backend/src/main/java/com/example/aprendendosembullying/repository/DepoimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aprendendosembullying.repository;
-
-import com.example.aprendendosembullying.model.Depoimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepoimentoRepository extends JpaRepository<Depoimento, Long> {
-}
